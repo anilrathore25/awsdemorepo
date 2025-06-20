3c73a30bba4529d3aa08ecf55d09d1b59e99abba
anilrathore25 <anilrathore3035@gmail.com>
2025-06-20 12:42:45 +0530

    Update AwstestApplication.java

--- src/main/java/com/yash/awstest/AwstestApplication.java
+++ src/main/java/com/yash/awstest/AwstestApplication.java
@@ -1,11 +1,15 @@
+
 package com.yash.awstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
 @SpringBootApplication
 public class AwstestApplication {
-	@GetMapping("welcome")
+	@GetMapping("/welcome")
 	public String Demo(){
 		return" hello this is the testing of deploying the cicd in aws";
 	}
